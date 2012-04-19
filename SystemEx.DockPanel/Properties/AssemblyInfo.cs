d9a2695bfb339ac9a1b46adc63ae65c5d0015abf
Pieter van Ginkel <pvginkel@gmail.com>
2012-04-19 09:48:18 +0200

    * Added unit test project;
    * Released 12.4.19.2.

--- SystemEx.DockPanel/Properties/AssemblyInfo.cs
+++ SystemEx.DockPanel/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("b035e0db-0c4e-4ce7-b294-3697e186579f")]
 
-[assembly: AssemblyVersion("12.4.15.1")]
-[assembly: AssemblyFileVersion("12.4.15.1")]
+[assembly: AssemblyVersion("12.4.19.2")]
+[assembly: AssemblyFileVersion("12.4.19.2")]
---
+++ SystemEx.Test/EscapingFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SystemEx.Test
+{
+    [TestFixture]
+    public class EscapingFixture
+    {
+        [Test]
+        public void StringEncoding()
+        {
+            Assert.AreEqual("\"a\\ra\"", Escaping.StringEncode("a\ra"));
+            Assert.AreEqual("\"a\\na\"", Escaping.StringEncode("a\na"));
+            Assert.AreEqual("\"a\\ta\"", Escaping.StringEncode("a\ta"));
+        }
+    }
+}
