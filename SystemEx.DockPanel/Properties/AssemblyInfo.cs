62a9375b79e5d37dd0a5023b2aec7681855390b1
Pieter van Ginkel <pginkel@gmt.nl>
2016-03-26 16:41:27 +0100

    Created version 16.3.26.6.

--- SystemEx.DockPanel/Properties/AssemblyInfo.cs
+++ SystemEx.DockPanel/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("b035e0db-0c4e-4ce7-b294-3697e186579f")]
 
-[assembly: AssemblyVersion("14.5.31.5")]
-[assembly: AssemblyFileVersion("14.5.31.5")]
+[assembly: AssemblyVersion("16.3.26.6")]
+[assembly: AssemblyFileVersion("16.3.26.6")]
--- SystemEx/Properties/AssemblyInfo.cs
+++ SystemEx/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("25f7d5c3-51d0-42a9-9d6e-0c702c944368")]
 
-[assembly: AssemblyVersion("14.5.31.5")]
-[assembly: AssemblyFileVersion("14.5.31.5")]
+[assembly: AssemblyVersion("16.3.26.6")]
+[assembly: AssemblyFileVersion("16.3.26.6")]
 
 [assembly: InternalsVisibleTo("SystemEx.DockPanel, PublicKey=00240000048000009400000006020000002400005253413100040000010001002bd439c64abaf8c4179bbc29ac76610fd33113cf928536c2b7189e438cf234c029bcc249565e35be811d4a6060baf8e49c4539bcf776c69f819c48b2ef37999e32e2fb7f97a3577c6f9db48b66bde4e8d74efa7b00debdb7fee506b281ab2541501fe2c73701642668ab9655e9f392f8126da9a1b5f3d8c9105c11e22cf6a6ce")]
