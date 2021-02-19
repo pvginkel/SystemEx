0b740c3006b6735e933b820d7c03d508f3e93b4b
Pieter van Ginkel <pvginkel@gmail.com>
2021-02-19 22:22:59 +0100

    Associated multi line text editor with the form header.

--- SystemEx/Windows/Forms/FormHeader.cs
+++ SystemEx/Windows/Forms/FormHeader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Drawing.Text;
 using System.Text;
 using System.Windows.Forms;
@@ -83,6 +84,7 @@ namespace SystemEx.Windows.Forms
         [Category("Appearance")]
         [Browsable(true)]
         [DefaultValue("")]
+        [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
         public override string Text
         {
             get { return _text; }
@@ -96,6 +98,7 @@ namespace SystemEx.Windows.Forms
         [Category("Appearance")]
         [Browsable(true)]
         [DefaultValue("")]
+        [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
         public string SubText
         {
             get { return _subText; }
