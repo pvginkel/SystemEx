3190819d08027b26a520688a15b0efc2bdef7568
Pieter van Ginkel <pvginkel@gmail.com>
2021-02-21 21:56:55 +0100

    Created version 21.2.21.10.

--- SystemEx.DockPanel/Properties/AssemblyInfo.cs
+++ SystemEx.DockPanel/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("b035e0db-0c4e-4ce7-b294-3697e186579f")]
 
-[assembly: AssemblyVersion("21.2.19.9")]
-[assembly: AssemblyFileVersion("21.2.19.9")]
+[assembly: AssemblyVersion("21.2.21.10")]
+[assembly: AssemblyFileVersion("21.2.21.10")]
